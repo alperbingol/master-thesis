052180eb1f5112b5a548454019b0b9b08093d7e9
alperbingol <alperbingol@sabanciuniv.edu>
2024-06-09 21:06:55 +0200

    created expectation class  and expectations using the class

---
+++ expectation/Expectation.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class Expectation(ABC):
+    def __init__(self, column, dimension, add_info = {}):
+        self.columns = column #name of the column on which the expectation rule will be applied
+        self.dimension = dimension #Completeness, Uniqueness, Validity, Accuracy, and Consistency
+        self.add_info = add_info #optional parameter for adding any additional information required for the test
+    
+    @abstractmethod
+    def test(self, ge_df):
+        pass
---
+++ expectation/ValuesInListExpectation.py
@@ -0,0 +1,8 @@
+from expectation.Expectation import Expectation
+
+class ValuesInListExpectation(Expectation):
+    def __init__(self, column, dimension, add_info = {}):
+        super().__init__(column, dimension, add_info)
+    
+    def test(self, ge_df):
+        ge_df.expect_column_distinct_values_to_be_in_set(column = self.columns, value_set = self.add_info['value_set'], meta = {'dimension': self.dimension})
